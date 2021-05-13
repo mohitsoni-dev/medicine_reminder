e73af5b8f4e101666eb7ccf4d6635b92704e1fe8
Mohit Soni <25.mohitsoni@gmail.com>
2021-05-13 19:14:08 +0530

    Made some edits in patienaddform.dart

--- lib/PatientAddForm.dart
+++ lib/PatientAddForm.dart
@@ -1,8 +1,4 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:path/path.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:sqflite/sqflite.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 
 class PatientAddForm extends StatefulWidget {
@@ -15,11 +11,12 @@ class _PatientAddFormState extends State<PatientAddForm> {
   void initState() {
     super.initState();
   }
+
   var data;
   bool autoValidate = true;
   bool readOnly = false;
   bool showSegmentedControl = true;
-  final  _fbKey = GlobalKey<FormBuilderState>();
+  final _fbKey = GlobalKey<FormBuilderState>();
 
   @override
   Widget build(BuildContext context) {
@@ -28,91 +25,92 @@ class _PatientAddFormState extends State<PatientAddForm> {
         title: Text("Patient's Data"),
       ),
       body: Padding(
-        padding: EdgeInsets.all(10),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              FormBuilder(
-                key: _fbKey,
-                initialValue: {
-                  'date': DateTime.now(),
-                  'accept_terms': false,
-                },
-                autovalidateMode: AutovalidateMode.always,
-                child: Column(
-                  children: [
-                    FormBuilderTextField(
-                      name: 'name',
-                      style: Theme.of(context).textTheme.body1,
-                      validator: FormBuilderValidators.compose([FormBuilderValidators.required(context)]),
-                      decoration: InputDecoration(labelText: "Full Name"),
-                    ),
-
-                    // FormBuilderDropdown(
-                    //   name: "gender",
-                    //   style: Theme.of(context).textTheme.body1,
-                    //   decoration: InputDecoration(labelText: "Gender"),
-                    //   // initialValue: 'Male',
-                    //   hint: Text('Select Gender'),
-                    //   validator: FormBuilderValidators.compose([FormBuilderValidators.required(context)]),
-                    //   items: ['Male', 'Female', 'Other']
-                    //       .map((gender) => DropdownMenuItem(
-                    //       value: gender, child: Text("$gender")))
-                    //       .toList(),
-                    // ),
-                    FormBuilderTextField(
-                      name: "age",
-                      style: Theme.of(context).textTheme.body1,
-                      decoration: InputDecoration(labelText: "Age"),
-                      keyboardType: TextInputType.number,
-                      validator:FormBuilderValidators.compose( [
-                        FormBuilderValidators.numeric(context),
-                        FormBuilderValidators.max(context,70),
-                      ]),
-                    ),
-                    FormBuilderTextField(
-                      name: 'condition',
-                      style: Theme.of(context).textTheme.body1,
-                      validator: FormBuilderValidators.compose([FormBuilderValidators.required(context)]),
-                      decoration: InputDecoration(labelText: "Condition"),
-                    ),
-                    FormBuilderTextField(
-                      name: 'doctor',
-                      style: Theme.of(context).textTheme.body1,
-                      validator: FormBuilderValidators.compose([FormBuilderValidators.required(context)]),
-                      decoration: InputDecoration(labelText: "Name of Doctor"),
-                    ),
-                  ],
-                ),
-              ),
-              Row(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            FormBuilder(
+              key: _fbKey,
+              initialValue: {
+                'date': DateTime.now(),
+                'accept_terms': false,
+              },
+              autovalidateMode: AutovalidateMode.always,
+              child: Column(
                 children: [
-                  MaterialButton(
-                    child: Text("Submit",style: TextStyle(color: Colors.white),),
-                    color: Colors.green,
-                    onPressed: () {
-                      _fbKey.currentState.save();
-                      if (_fbKey.currentState.validate()) {
-                        print(_fbKey.currentState.value);
-                      }
-                    },
+                  FormBuilderTextField(
+                    name: 'name',
+                    style: Theme.of(context).textTheme.bodyText2,
+                    validator: FormBuilderValidators.compose(
+                        [FormBuilderValidators.required(context)]),
+                    decoration: InputDecoration(labelText: "Full Name"),
                   ),
-                  MaterialButton(
-                    child: Text("Reset",style: TextStyle(color: Colors.white),),
-                    color: Colors.deepOrange,
-                    onPressed: () {
-                      _fbKey.currentState.reset();
-                    },
+                  SizedBox(height: 8.0),
+                  FormBuilderTextField(
+                    name: "age",
+                    style: Theme.of(context).textTheme.bodyText2,
+                    decoration: InputDecoration(labelText: "Age"),
+                    keyboardType: TextInputType.number,
+                    validator: FormBuilderValidators.compose([
+                      FormBuilderValidators.numeric(context),
+                      FormBuilderValidators.max(context, 110),
+                    ]),
+                  ),
+                  SizedBox(height: 8.0),
+                  FormBuilderTextField(
+                    name: 'condition',
+                    style: Theme.of(context).textTheme.bodyText2,
+                    // validator: FormBuilderValidators.compose(
+                    //     [FormBuilderValidators.required(context)]),
+                    decoration: InputDecoration(labelText: "Condition"),
+                  ),
+                  SizedBox(height: 8.0),
+                  FormBuilderTextField(
+                    name: 'doctor',
+                    style: Theme.of(context).textTheme.bodyText2,
+                    // validator: FormBuilderValidators.compose(
+                    //     [FormBuilderValidators.required(context)]),
+                    decoration: InputDecoration(labelText: "Name of Doctor"),
                   ),
                 ],
-              )
-            ],
-          ),
+              ),
+            ),
+            SizedBox(height: 16.0),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                MaterialButton(
+                  height: 40.0,
+                  child: Text(
+                    "Submit",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  color: Colors.green,
+                  onPressed: () {
+                    _fbKey.currentState.save();
+                    if (_fbKey.currentState.validate()) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text('Processing Data')));
+                      print(_fbKey.currentState.value);
+                    }
+                  },
+                ),
+                MaterialButton(
+                  height: 40.0,
+                  child: Text(
+                    "Reset",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  color: Colors.deepOrange,
+                  onPressed: () {
+                    _fbKey.currentState.reset();
+                  },
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
   }
 }
-
