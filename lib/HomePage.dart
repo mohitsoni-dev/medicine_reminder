ae4c23d7432e850c70e782caf1097f4432f429db
Yaksh <sharmayaksh945@gmail.com>
2021-05-14 15:19:23 +0530

    Updated HomePage.dart Drawer Class.

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:meds_reminder/DrawerClass.dart';
 import 'package:meds_reminder/PatientAddForm.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -15,9 +14,44 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text('Meds Reminder'),
       ),
       body: Center(
-        child: Text("Upcomming reminders are displayed here"),
+        child: Text("Upcoming reminders are displayed here"),
       ),
-      drawer: DrawerClass(),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: const <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Colors.purple, Colors.indigo],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Patient\'s Name',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.more),
+              title: Text('More Details'),
+            ),
+            ListTile(
+              leading: Icon(Icons.edit),
+              title: Text('Edit Details'),
+            ),
+            ListTile(
+              leading: Icon(Icons.info_outline),
+              title: Text('About'),
+            ),
+          ],
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(context,
