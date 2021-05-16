b4a76045a6046629872293bbe2377f23266eb979
shivlendra20 <shivlendra14@gmail.com>
2021-05-16 07:49:22 +0530

    Added NavigationBar to HomePage.dart

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -8,6 +8,34 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'It will display your schedule',
+      style: optionStyle,
+    ),
+    Text(
+      'It will displays your medications',
+      style: optionStyle,
+    ),
+    Text(
+      'It will display your Settings',
+      style: optionStyle,
+    ),
+    Text(
+      'It will display your files',
+      style: optionStyle,
+    )
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,9 +43,10 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text('Meds Reminder'),
       ),
       body: Center(
-        child: Text("Upcoming reminders are displayed here"),
+        child: _widgetOptions.elementAt(_selectedIndex),
       ),
       drawer: DrawerClass(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(context,
@@ -25,6 +54,42 @@ class _MyHomePageState extends State<MyHomePage> {
         },
         tooltip: 'Add a patient',
         child: Icon(Icons.add),
+      ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+              color: Colors.white,
+              blurRadius: 10,
+            ),
+          ],
+        ),
+        child: BottomNavigationBar(
+          elevation: 10.0,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.access_time),
+              label: 'Schedule',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.medical_services),
+              label: 'Medications',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'Settings',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.folder),
+              label: 'Files',
+            ),
+          ],
+          type: BottomNavigationBarType.fixed,
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.blue[600],
+          unselectedItemColor: Colors.blue[200],
+          onTap: _onItemTapped,
+        ),
       ),
     );
   }
