3d4d50ab9f248747c3e6e0978698c2df9ccc26da
Rakshit Verma <rakshitvermajpr@gmail.com>
2021-06-17 22:52:30 +0530

    reminder db and table

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:meds_reminder/database/models/Patient.dart';
+import 'package:meds_reminder/database/models/Reminder.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
@@ -24,7 +25,17 @@ class DatabaseProvider {
         onCreate: (Database db, int version) async {
       await db.execute(
           "CREATE TABLE Patient(id integer primary key AUTOINCREMENT, name TEXT, age integer, doctor TEXT, condition TEXT)");
-    });
+      await db.execute(
+          "CREATE TABLE Reminder(id integer primary key AUTOINCREMENT, medname TEXT, timings integer)");
+      await db.execute(
+          "INTER INTO Reminder values(\"Limcee\", 3)");
+      await db.execute(
+          "INTER INTO Reminder values(\"Doxy 650\", 2)");
+      await db.execute(
+          "INTER INTO Reminder values(\"Ivermictlin\", 1)");
+
+        });
+
   }
 
   addPatientToDatabase(Patient patient) async {
@@ -49,5 +60,11 @@ class DatabaseProvider {
     final db = await database;
     var response = await db.query("Patient", where: "id = ?", whereArgs: [id]);
     return response.isNotEmpty ? Patient.fromMap(response.first) : null;
+  }
+  Future<List<Reminder>> getAllReminders() async {
+    final db = await database;
+    var response = await db.query("Reminder");
+    List<Reminder> list = response.map((c) => Reminder.fromMap(c)).toList();
+    return list;
   }
 }
